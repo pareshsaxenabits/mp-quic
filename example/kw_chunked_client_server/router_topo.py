00c0040688d3797463d5eda22f83a40313924434
Kaustubh Welankar <kaustubh0x77@gmail.com>
2019-10-26 16:22:20 +0530

    Added new way of making a network for ease of use

--- example/kw_chunked_client_server/router_topo.py
+++ example/kw_chunked_client_server/router_topo.py
@@ -1,4 +1,55 @@
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+from time import sleep
+
+
+def multipathTopo():
+    net = Mininet(build = False)
+    # net = Mininet( controller=RemoteController, link=TCLink, switch=OVSKernelSwitch )
+    
+    c0 = net.addController("c0")
+    
+    # server = net.addHost('server', ip='100.0.0.1/8')
+    # client = net.addHost('client', ip='10.0.0.1/8')
+    # router = net.addHost('router', ip='10.0.0.2/8')
+    server = net.addHost('server')
+    client = net.addHost('client')
+    router = net.addHost('router')
+
+    leftSwitch = net.addSwitch( 's1' )
+    rightSwitch = net.addSwitch( 's2' )
+    serverSwitch = net.addSwitch( 's3' )
+        
+    net.addLink(client, leftSwitch)
+    net.addLink(client, rightSwitch)
+
+    net.addLink(router,leftSwitch)
+    net.addLink(router,rightSwitch)
+    net.addLink(router,serverSwitch) 
+    net.addLink(serverSwitch,server)
+
+    net.build()
+
+    server.cmd('./server/config.sh')
+    client.cmd('./client/config.sh')
+    router.cmd('./router/config.sh')
+
+    controller = net.controllers[0]
+    controller.start()
+    
+    leftSwitch.start([controller])
+    rightSwitch.start([controller])
+    serverSwitch.start([controller])
+
+    return net
 
 class MyTopo( Topo ):
     "Simple topology example."
@@ -38,3 +89,8 @@ class MyTopo( Topo ):
         # rightHost.setIP("10.0.1.2", intf='h2-eth1') 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
+
+if __name__ == "__main__":
+    net = multipathTopo()
+    CLI(net)
+
