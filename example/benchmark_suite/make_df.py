9a3b32788db7f118ea0aa193545ce1d295ba7309
Paresh <paresh@example.com>
2020-04-05 23:43:45 +0530

    Major Changes for Desired Output

--- example/benchmark_suite/make_df.py
+++ example/benchmark_suite/make_df.py
@@ -5,12 +5,12 @@ import re
 import json
 import pandas as pd
 
-EXPERIMENTS = range(1,26)
+EXPERIMENTS = range(1,3)
 EXPERIMENT_DIR_ROOT = './experiments/'
 RESULTS_DIR = './results/'
 PROTOCOLS = ['mpquic_lowestrtt', 'mpquic_roundrobin', 'mptcp_default', 'mptcp_redundant', 'quic', 'tcp']
-
-def getExperimentSummary(exp_id):
+TotalProto = range(1,len(PROTOCOLS)+1)
+def getExperimentSummary(exp_id,proto_id):
     df_row = dict()
 
     exp_dir = EXPERIMENT_DIR_ROOT + str(exp_id) + "/"
@@ -20,8 +20,9 @@ def getExperimentSummary(exp_id):
         experiment_settings = yaml.load(f)
         for c in experiment_settings:
             if c != 'varying_qdisc':
-                df_row[c] = experiment_settings[c]
-
+                df_row['unknown'] = experiment_settings[c]
+    df_row['Experiment id'] = exp_id
+    df_row['Protocol'] = PROTOCOLS[proto_id-1]
     exp_qdisc_yaml = exp_dir + 'qdisc.yaml'
     with open(exp_qdisc_yaml) as f:
         config_data = yaml.load(f)
@@ -31,28 +32,29 @@ def getExperimentSummary(exp_id):
             df_row[interface+'_loss'] = interface_data['netem']['packet_loss']['loss']
             df_row[interface+'_bandwidth'] = interface_data['tbf']['rate']
     
-    regex = re.compile(str(exp_id)+'_.*')
+    regex = re.compile(str(exp_id)+'-.*')
     match_files = [o for o in os.listdir(RESULTS_DIR) if regex.match(o)]
     assert( len(match_files) == 1 )
     
     result_dir = RESULTS_DIR + match_files[0]
-    for proto in PROTOCOLS:
-        report_file = result_dir + '/' + proto + '/report.json'
-        with open(report_file, 'r') as f:
-            run_stats = json.load(f)
+    #for proto in PROTOCOLS:
+    report_file = result_dir + '/' + PROTOCOLS[proto_id-1] + '/report.json'
+    with open(report_file, 'r') as f:
+        run_stats = json.load(f)
     
     df_row['Time'] = run_stats['Total time taken (s)']
     df_row['s1_total'] = run_stats['Transfers (Bytes)']['total']['s1-eth1']
     df_row['s2_total'] = run_stats['Transfers (Bytes)']['total']['s2-eth1']
-    df_row['goodput'] = int(1024 * 1024 * run_stats['Goodput (MBytes/s)'] * run_stats['Total time taken (s)'])
+    df_row['goodput'] = run_stats['Goodput (Mbits/s)']
     return df_row
 
 
 if __name__ == "__main__":
     # df = pd.DataFrame(columns = [])
     data = []
-    for i in range(1,27):
-        data.append(getExperimentSummary(i))
+    for i in EXPERIMENTS: 
+        for j in TotalProto:
+            data.append(getExperimentSummary(i,j))
 
     df = pd.DataFrame(data)
     df.to_csv('results/summary.csv')
