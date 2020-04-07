5fe9e25bfa33f852f5aae2415f24a4ef459d1790
Paresh <psaxena@hyderabad.bits-pilani.ac.in>
2020-04-08 00:09:32 +0530

    supporting bigger files

--- example/benchmark_suite/utils/tcp_server.go
+++ example/benchmark_suite/utils/tcp_server.go
@@ -8,7 +8,7 @@ import (
 
 const addr = "0.0.0.0:4343"
 
-var fakeBuff = make([]byte, 1024*1024*10) // 10 MB
+var fakeBuff = make([]byte, 1024*1024*100) // 100 MB
 
 func main() {
 	for i := 0; i < cap(fakeBuff); i++ {
